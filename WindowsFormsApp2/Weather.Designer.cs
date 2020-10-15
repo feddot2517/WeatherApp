caa567d5c74eab12275fedf0cb23aa444a4c67d0
Michail Ivanovskii <feddot2517@outlook.com>
2020-10-15 20:55:18 +0600

    Изменен экшен при нажатии на кнопку поиск. Происходит пост запрос на апи с погодой, результат парсится и показывается пользователю

--- WindowsFormsApp2/Weather.Designer.cs
+++ WindowsFormsApp2/Weather.Designer.cs
@@ -30,29 +30,62 @@
         {
             this.inputCity = new System.Windows.Forms.TextBox();
             this.search = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.helpMessage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // inputCity
             // 
-            this.inputCity.Location = new System.Drawing.Point(13, 13);
+            this.inputCity.Location = new System.Drawing.Point(12, 30);
             this.inputCity.Name = "inputCity";
             this.inputCity.Size = new System.Drawing.Size(255, 20);
             this.inputCity.TabIndex = 0;
             // 
             // search
             // 
-            this.search.Location = new System.Drawing.Point(274, 12);
+            this.search.Location = new System.Drawing.Point(273, 29);
             this.search.Name = "search";
             this.search.Size = new System.Drawing.Size(76, 23);
             this.search.TabIndex = 1;
             this.search.Text = "Найти";
             this.search.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 69);
+            this.label1.MaximumSize = new System.Drawing.Size(334, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Здесь будет погода";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // helpMessage
+            // 
+            this.helpMessage.AutoSize = true;
+            this.helpMessage.Location = new System.Drawing.Point(12, 11);
+            this.helpMessage.Name = "helpMessage";
+            this.helpMessage.Size = new System.Drawing.Size(81, 13);
+            this.helpMessage.TabIndex = 4;
+            this.helpMessage.Text = "Введите город";
+            // 
             // Weather
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(361, 272);
+            this.Controls.Add(this.helpMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.search);
             this.Controls.Add(this.inputCity);
             this.Name = "Weather";
@@ -66,6 +99,9 @@
 
         private System.Windows.Forms.TextBox inputCity;
         private System.Windows.Forms.Button search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label helpMessage;
     }
 }
 
--- WindowsFormsApp2/Weather.cs
+++ WindowsFormsApp2/Weather.cs
@@ -8,11 +8,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.CodeDom.Compiler;
+using System.Windows.Forms.VisualStyles;
 
 namespace WindowsFormsApp2
 {
+    class WeatherInterfaceMain
+    {
+        public double temp;
+        public double pressure;
+        public double feels_like;
+    }
+    class WeatherInterface
+    {
+        public WeatherInterfaceMain main;
+    }
     public partial class Weather : Form
     {
+        public double getCelsiaFromFarengeitString(double t)
+        {
+            return t - 273.15;
+        }
         public Weather()
         {
             InitializeComponent();
@@ -21,10 +40,28 @@ namespace WindowsFormsApp2
         public void onLoadEnd(object sender, EventArgs e)
         {
             search.Click += onClickHandler;
+
         }
-        private void onClickHandler(object sender, EventArgs e)
+        private async void onClickHandler(object sender, EventArgs e)
         {
-            Debug.Print(inputCity.Text);
+            WebRequest request = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q="+inputCity.Text+"&APPID=e8c94dad7f4ae805e4df3e195d3d2eb6");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-urlencoded";
+            WebResponse response = await request.GetResponseAsync();
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(s))
+                {
+                    string weatherJson = await reader.ReadToEndAsync();
+                    var currentWeather = JsonConvert.DeserializeObject<WeatherInterface>(weatherJson);
+                    label1.Text = "Погода в " + inputCity.Text + ":\n\n";
+                    label1.Text += "Температура C* "+getCelsiaFromFarengeitString(currentWeather.main.temp).ToString("#") + "\n";
+                    label1.Text += "Ощущается как C* " + getCelsiaFromFarengeitString(currentWeather.main.feels_like).ToString("#") + "\n";
+                    label1.Text += "Давление " + getCelsiaFromFarengeitString(currentWeather.main.pressure).ToString("#") + "\n";
+                }
+            }
+            response.Close();
+
         }
     }
 }
